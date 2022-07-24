b4282d1f4cc4ec5d922d3ecd9cf8943976776dae
RicardoChairez <rdc257@nau.edu>
2022-07-24 11:56:11 -0700

    Adds view my teams resolves #8

---
+++ PokePicker/PokePicker/Controllers/MyTeamsVC.swift
@@ -0,0 +1,29 @@
+//
+//  MyTeamsVC.swift
+//  PokePicker
+//
+//  Created by Chip Chairez on 7/24/22.
+//
+
+import UIKit
+
+class MyTeamsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PokePicker/PokePicker/Controllers/TeamCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamCell.swift
+//  PokePicker
+//
+//  Created by Chip Chairez on 7/24/22.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
